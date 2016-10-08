6a4011c1c62d5c0eddf694f4e176c18a7f5c083a
Jonck <jonckjuniorr@gmail.com>
2016-10-07 22:03:56 -0300

    Update UVa 11951 - Area.cpp

--- UVa 11951 - Area.cpp
+++ UVa 11951 - Area.cpp
@@ -1,41 +1,42 @@
-#include <iostream>
-#include <cstdio>
 #include <algorithm>
-#include <cmath>
-#include <vector>
+#include <cstdio>
+#include <stack>
+#include <iostream>
 using namespace std;
 
 long long int grid[105][105];
 
 int main() {
-    int t, caso = 0;
+    int t,n,m,b,caso=0;
     cin >> t;
     while(t--){
-        int n,m,b;
         cin >> n >> m >> b;
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++){
                 scanf("%lld",&grid[i][j]);
-                if(i > 0) grid[i][j] += grid[i-1][j];
-                if(j > 0) grid[i][j] += grid[i][j-1];
-                if(i > 0 && j >0) grid[i][j] -= grid[i-1][j-1];
+                if(i>0) grid[i][j] += grid[i-1][j];
+            }   
+
+        long long int budget=1e8, subRect, area, areamax = 0, start;
+        for(int l = 0; l < n; l++)
+            for(int i = l; i < n; i++){
+                subRect = 0;
+                start = 0;
+                for(int j = 0; j < m; j++){
+                    subRect += grid[i][j];
+                    if(l>0) subRect -= grid[l-1][j];
+                    for(; start < j && subRect > b; start++){
+                        subRect -= grid[i][start];
+                        if(l>0) subRect += grid[l-1][start];
+                    }
+                    area = (i-l+1)*(j-start+1);
+                    if(area > areamax)
+                        areamax = area, budget = subRect;
+                    else if(area == areamax)
+                        budget = min(budget, subRect);
+                }
             }
-        long long int subRec,area, maxArea = 0, budget = 0;
-        for(int i = 0; i < n; i++) for(int j = 0; j < m; j++)
-        for(int k = i; k < n; k++) for(int l = j; l < m; l++){
-            subRec = grid[k][l];
-            if(i > 0) subRec -= grid[i-1][l];
-            if(j > 0) subRec -= grid[k][j-1];
-            if(i > 0 && j >0) subRec += grid[i-1][j-1];
-            if(subRec > b) break;
-            area = (k-i+1)*(l-j+1);
-            if(area > maxArea){
-                maxArea = area;
-                budget = subRec;
-            }
-            else if(area == maxArea && subRec < budget) budget = subRec;
-        }
-        printf("Case #%d: %lld %lld\n",++caso,maxArea,budget);
+        printf("Case #%d: %lld %lld\n",++caso,areamax, budget);
     }
     return 0;
 }

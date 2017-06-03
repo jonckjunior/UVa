522e4cd082c76f05ee438d4f87f43aaa3eccef34
Jonck <jonckjuniorr@gmail.com>
2017-06-02 22:02:55 -0300

    Update URI 1166 - Torre de Hanoi, novamente!.cpp

--- URI 1166 - Torre de Hanoi, novamente!.cpp
+++ URI 1166 - Torre de Hanoi, novamente!.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
-
+// Foi usado uma abordagem gulosa, tentando colocar em ordem crescente uma bola em cada torre Ti até Tn
+// Quando não era possível colocar uma bola em uma determinada torre Ti, pulasse para a proxima torre
+// Caso chegue na Tn e ainda não adicionou a bola, o programa termina
+// Complexidade de Tempo O(mn) onde n são as torres e m é o número máximo de bolas que é possivel adicionar
+// Complexidade de Espaço O(m+n) onde serão criados n pilhas, e serão distribuidas entre as n pilhas as m bolas
 using namespace std;
 
 vector< stack<int> > vs;
@@ -15,9 +19,9 @@ int main(){
     cin >> t;
     while(t--){
         cin >> n;
-        vs.assign(n+1, stack<int> ());
+        vs.assign(n+1, stack<int> ()); // O(n)
         int bola = 1;
-        for(int i = 0; i < n ; i++){
+        for(int i = 0; i < n ; i++){ // O(mn) para cada bola m ira percorrer o for
             if(vs[i].empty())
                 vs[i].push(bola++), i = -1;
             else{
